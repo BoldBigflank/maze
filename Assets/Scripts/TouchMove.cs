ab558175ab1a310793fe9d88d044814180a21a65
Alex Swan <alex@bold-it.com>
2015-12-09 01:03:39 -0800

    Touch to move

---
+++ Assets/Scripts/TouchMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class TouchMove : MonoBehaviour {
+
+	public float speed = 6.0F;
+    public Camera forwardCamera;
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 forward = forwardCamera.transform.forward;
+        float curSpeed = (Input.touchCount > 0 || Input.GetMouseButton(0)) ? speed : 0.0f;
+        controller.SimpleMove(forward * curSpeed);
+    }
+
+    void onGui(){
+    	GUI.Label(new Rect(10, 10, 100, 100), "Hello");
+
+    }
+}
